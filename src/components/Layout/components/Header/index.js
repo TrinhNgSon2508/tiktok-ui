cbec182b3d04e89ec7255f2345df73e215e5f994
sonngoc2502 <56708950+sonngoc2502@users.noreply.github.com>
2022-09-09 17:17:48 +0700

    Dựng khung Layout Default

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind';
+import style from './Header.module.scss';
+
+const cx = classNames.bind(style);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wapper')}>
+            <div className={cx('inner')}>
+                {/*Logo */}
+                {/*Seach */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
