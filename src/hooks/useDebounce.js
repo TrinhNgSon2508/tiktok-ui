d7923dd1753ae46d03bf89080be81cc222d437b9
TrinhNgSon2508 <56708950+TrinhNgSon2508@users.noreply.github.com>
2022-09-15 17:16:48 +0700

    Sửa lỗi phần Header #2

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -6,6 +6,7 @@ function useDebounce(value, delay) {
     useEffect(() => {
         const handler = setTimeout(() => setDebounceValue(value), delay);
         return () => clearTimeout(handler);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value]);
     return debounceValue;
 }
