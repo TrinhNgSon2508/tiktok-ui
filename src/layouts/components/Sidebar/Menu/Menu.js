2e7794a9eb6171b5821e26292961f415f2584fd1
TrinhNgSon2508 <56708950+TrinhNgSon2508@users.noreply.github.com>
2022-09-17 14:53:13 +0700

    UI phần sidebar #1

---
+++ src/layouts/components/Sidebar/Menu/Menu.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+function Menu({ children }) {
+    return <nav>{children}</nav>;
+}
+Menu.prototype = {
+    children: PropTypes.node.isRequired,
+};
+export default Menu;
